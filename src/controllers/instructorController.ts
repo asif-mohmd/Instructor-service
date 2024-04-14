7aa2acb09f2e329efa106fe819ae1d119e0b5bc8
asif-mohmd <asifasifpsps@gmail.com>
2024-04-14 10:01:18 +0530

    Changed to class based approach

--- src/controllers/instructorController.ts
+++ src/controllers/instructorController.ts
@@ -1,82 +1,77 @@
 import { IInstructorInteractor } from "../interfaces/IInstructorInteractor";
 
 export class InstructorController {
+  private interactor: IInstructorInteractor;
 
-    private interactor: IInstructorInteractor;
+  constructor(interactor: IInstructorInteractor) {
+    this.interactor = interactor;
+  }
 
-    constructor(interactor: IInstructorInteractor){
-        this.interactor = interactor
+  onRegister: any = async (call: any, callback: any) => {
+    try {
+      const request = call.request as {
+        name: string;
+        email: string;
+        password: string;
+      };
+      const response = await this.interactor.instructorRegister(request);
+      if (response.registerStatus) {
+        callback(null, {
+          msg: "OTP send",
+          registerStatus: true,
+          instructorData: response.activationToken,
+        });
+      } else {
+        callback(null, {
+          msg: "User Already exists",
+          registerStatus: false,
+        });
+      }
+    } catch (error) {
+      callback(error);
     }
+  };
 
-    onRegister: any = async (call: any, callback: any) => {
-        try {
-
-          const request = call.request as {
-            name: string;
-            email: string;
-            password: string;
-          };
-          const response = await this.interactor.instructorRegister(request);
-          if (response.registerStatus) {
-            callback(null, {
-              msg: "OTP send",
-              registerStatus: true,
-              instructorData: response.activationToken,
-            });
-          } else {
-            callback(null, {
-              msg: "User Already exists",
-              registerStatus: false,
-            });
-          }
-        } catch (error) {
-          callback(error);
-        }
+  onActivateInstructor: any = async (call: any, callback: any) => {
+    try {
+      console.log("otp");
+      const request = call.request as {
+        name: string;
+        email: string;
+        password: string;
       };
+      const response = await this.interactor.activateInstructor(request);
+      if (response) {
+        callback(null, {
+          status: true,
+        });
+      } else {
+        callback(null, {
+          status: false,
+        });
+      }
+    } catch (err) {
+      callback(err);
+    }
+  };
 
-
-      onActivateInstructor : any = async (call: any , callback: any) =>{
-        try {
-          console.log("otp")
-          const request = call.request as {
-            name: string;
-            email: string;
-            password: string;
-          };
-          const response = await this.interactor.activateInstructor(request);
-          if (response) {
-            callback(null, {
-              status: true,
-            });
-          } else {
-            callback(null, {
-              status: false,
-            });
-          }
-        } catch (err) {
-          callback(err);
-        }
+  onLogin: any = async (call: any, callback: any) => {
+    try {
+      console.log("hereeeeeeeeeeeeeeeeeeeeeeee");
+      const { email, password } = call.request as {
+        email: string;
+        password: string;
       };
-
-      onLogin : any = async(call:any , callback: any) =>{
-        try {
-          console.log("hereeeeeeeeeeeeeeeeeeeeeeee")
-          const { email, password } = call.request as {
-            email: string;
-            password: string;
-          };
-          const response = await this.interactor.instructorLogin(email, password);
-    console.log(response,"resssssssssssssssssssssssss")
-          callback(null, {
-            msg: response.msg,
-            status: response.status,
-            otp: response.otp,
-            loginStatus: response.loginStatus,
-          });
-        } catch (error) {
-          callback(error);
-        }
-      };
-      
-
+      const response = await this.interactor.instructorLogin(email, password);
+      console.log(response, "resssssssssssssssssssssssss");
+      callback(null, {
+        msg: response.msg,
+        status: response.status,
+        otp: response.otp,
+        loginStatus: response.loginStatus,
+      });
+    } catch (error) {
+      callback(error);
+    }
+  };
 }
